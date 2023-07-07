b732ffae506759f74bf1540e64262833111c409e
Alexander Cascante <luisccascante@hotmail.com>
2023-07-07 17:50:36 -0600

    Version Final

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -26,20 +26,17 @@ const getUsersbyid = async (req,res)=>{
 const createUser = async (req,res)=>{
     const {nombre,apellido1,apellido2,telefono,cedula,direccion,tipopersona,estado} = req.body;
     const response = await pool.query('INSERT INTO PERSONAS (nombre,apellido1,apellido2,telefono,cedula,direccion,tipopersona,estado) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *',[nombre,apellido1,apellido2,telefono,cedula,direccion,tipopersona,estado]);
-    console.log(response.rows[0]['idpersona']);
-    /*if (tipopersona=true) {
-        pool.query('INSERT INTO USUARIOS (idPersona,userid,password,estado) VALUES ($1,$2,$3,$4) RETURNING *',[response.rows[0]['idpersona'],nombre.substring(0,1)+apellido1,'Welcome',true]);        
-    }
-    /*res.json({
-        message:'Usuario agregado',
-        body:{
-            user:{nombre,apellido1,apellido2,telefono,cedula,direccion,tipopersona,estado}
-        }
-    });*/
     res.status(200).json(response.rows);
 };
 
-const updateUser = async (req,res)=>{
+function createSistemUser(userid,username){
+    pool.query('INSERT INTO USUARIOS (idPersona,userid,password,estado) VALUES ($1,$2,$3,$4)',userid,username,'Welcome',true);
+    //console.log(response.rows);
+    console.log(username);
+    //res.status(200).json(response.rows);
+};
+
+const updateUser = async (req,res)=>{1
     const id = req.params.id;
     const {nombre,apellido1,apellido2,telefono,cedula,direccion,tipopersona,estado} = req.body;
     const response = await pool.query('UPDATE PERSONAS SET nombre = $1,apellido1 = $2,apellido2 = $3,telefono = $4,cedula = $5,direccion = $6,tipopersona = $7,estado = $8 WHERE idpersona = $9',[nombre,apellido1,apellido2,telefono,cedula,direccion,tipopersona,estado,id]);
